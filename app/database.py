5a3ed481555df8414fb15ac94b0aca6a3519b7e4
Wilmar Florez <wilmar.florez@elenas.la>
2025-04-22 16:14:31 -0500

    config alchemy orm first steps

---
+++ app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+import sys
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:turbodb@localhost/fastapi'
+
+try:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+    with engine.connect() as conn:
+        print("✅ Conexión exitosa a la base de datos.")
+except OperationalError as e:
+    print("❌ Error: Falló la conexión a la base de datos")
+    print("Detalle:", e)
+    sys.exit(1)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ app/main.py
@@ -0,0 +1,154 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional, List
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, SessionLocal
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class Post(BaseModel):
+    id: Optional[int] = 0
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="turbodb",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was succesfully")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error", error)
+        time.sleep(2)
+
+
+my_posts: List[dict] = [
+    Post(
+        title="Desayuno", content="Me gustan los huevos con tosineta", id=1
+    ).model_dump(),
+    Post(
+        title="Comidas rápidas", content="Me gusta la pizza hawaina", id=2
+    ).model_dump(),
+]
+
+
+def find_post(id: int):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
+def find_index_post(id: int):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+    return None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello there"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
+
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""select * from posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    cursor.execute(
+        """insert into posts (title, content, published) values (%s, %s, %s) returning *""",
+        (post.title, post.content, post.published),
+    )
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"detail": post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: str):
+    cursor.execute("""select * from posts where id=%s""", (str(id)))
+    post = cursor.fetchone()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} was not found",
+        )
+
+    return post
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_200_OK)
+def delete_post(id: int):
+    cursor.execute("""delete from posts where id = %s returning *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+
+    return {"message": f"Post with id: {id} was successfully deleted"}
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute(
+        """update posts set title = %s, content = %s, published = %s where id = %s returning *""",
+        (post.title, post.content, post.published, str(id)),
+    )
+
+    updated_post = cursor.fetchone()
+
+    if updated_post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+
+    return updated_post
