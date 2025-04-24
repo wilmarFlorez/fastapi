7bb6bffecf247038a98220ce5a05a6f776eafe74
Wilmar Florez <wilmar.florez@elenas.la>
2025-04-23 21:33:21 -0500

    add hassed password

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 
@@ -166,6 +166,10 @@ def update_post(id: int, new_post: schemas.PostCreate, db: Session = Depends(get
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    hashed_password = utils.hash_password(user.password)
+    user.password = hashed_password
+
     new_user = models.User(
         email=user.email,
         password=user.password,
